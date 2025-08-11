b4ef56daeac5f11231dbce32c9712e356959e860
Alirana2829 <alimahmoodrana00@gmail.com>
2025-08-12 00:25:09 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,12 @@ import { stripeWebHook } from "./controllers/stripeWebHooks.js";
 
 const app = express(); 
 
-app.use('api/stripe', express.raw({ type: 'application/json' }), stripeWebHook);
+app.use('/api/stripe', express.raw({ type: 'application/json' }), stripeWebHook);
 
 app.use(express.json());
 
 const corsOptions = {
-    origin: "https://moviemate-app-psi.vercel.app",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: "POST, GET, PUT, DELETE",
 };
 app.use(cors(corsOptions));
@@ -29,17 +29,14 @@ app.use(clerkMiddleware());
 
 // Routes
 app.get('/', (req, res) => res.send("Server is Live"));
-app.use("api/inngest", serve({ client: inngest, functions }));
-app.use("api/show", showRouter);
-app.use("api/booking", BookingRouter);
-app.use("api/admin", adminRouter);
-app.use("api/user", userRouter);
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/show", showRouter);
+app.use("/api/booking", BookingRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/user", userRouter);
 
 // Start server
 app.listen(process.env.PORT, () => {
     connectDB();
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
-
-
-
